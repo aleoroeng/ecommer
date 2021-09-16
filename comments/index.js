7da9dc3509875cd57276028f0b3ed06f2daed0eb
aleoroeng <oro99eng@gmail.com>
2021-09-15 21:14:49 -0400

    added react components and refactored comment and post service

--- comments/index.js
+++ comments/index.js
@@ -1,26 +1,29 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const cors = require('cors')
 const {randomBytes} = require('crypto')
+
 const app = express()
-app.use(bodyParser.json())
+app.use(express.json())
+app.use(cors())
 
 const port = 8000
 const commentsByPostId = {}
 
-app.get('/posts/:id/comments',(req,res)=>{
-    res.send(commentsByPostId[req.params.id] || [] ) 
+app.get('/posts/comments',(req,res)=>{
+    res.send(commentsByPostId[req.query['post_id']] || [] ) 
 })
 
-app.post('/posts/:id/comments',(req,res)=>{
+app.post('/posts/comments',(req,res)=>{
 
     const id = randomBytes(4).toString('hex')
     const content = req.body['content']
-    const postId = req.body['post_id']
+    const postId = req.query['post_id']
 
-    const comments = commentsByPostId[req.params.id] || []
+    const comments = commentsByPostId[postId] || []
     comments.push({id, content})
-    commentsByPostId[req.params.id] = comments
-    res.status(201).send(commentsByPostId[req.params.id])
+    commentsByPostId[postId] = comments
+
+    res.status(201).send(commentsByPostId[postId])
 })
 
 app.listen(port,()=>{
